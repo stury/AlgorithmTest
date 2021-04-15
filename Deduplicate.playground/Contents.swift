5b11c8d08c3c53fab5a3e09b02cfd9c722e83feb
Scott Tury <stury@mac.com>
2021-04-15 16:39:27 -0400

    Checking in a Dedupe Playground to show the time difference using two different deduping techniques.

---
+++ Deduplicate.playground/Contents.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+struct Metadata {
+    let shortName : String
+    let name : String
+    let id : String
+}
+
+func dedupe(_ filtered: [Metadata]) -> [Metadata] {
+    var lookup = Set<String?>()
+    var deduped = [Metadata]()
+    for metadata in filtered {
+        if lookup.contains(metadata.shortName) == false {
+            deduped.append(metadata)
+             lookup.insert(metadata.shortName)
+         }
+     }
+     return deduped
+}
+
+func dedupe2(_ filtered: [Metadata]) -> [Metadata] {
+    var deduped = [Metadata]()
+    for metadata in filtered {
+        if deduped.contains(where: { return $0.shortName == metadata.shortName }) == false {
+            deduped.append(metadata)
+         }
+     }
+     return deduped
+}
+
+func generateMetaData(_ numItems: Int ) -> [Metadata] {
+    var result = [Metadata]()
+    
+    for index in 0..<numItems {
+        result.append(Metadata(shortName: String(index), name: String(index), id: String(index)))
+    }
+    
+    return result
+}
+
+func time( _ work: ()->Void ) -> TimeInterval {
+    let startTimeInterval = Date().timeIntervalSince1970
+    work()
+    return Date().timeIntervalSince1970 - startTimeInterval
+}
+
+
+let metadata = generateMetaData(1000)
+print( time({
+    _ = dedupe(metadata)
+} ))
+
+print( time({
+    _ = dedupe2(metadata)
+} ))
